f74894c070c97c8d636323c823d3e761853788d7
NP <nb.piscitello@gmail.com>
2017-02-27 11:54:14 -0500

    added udev, overhauled arg processing, added remove function

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-#<TODO> Allow user to cherry-pick configs they want to install
-# (possible implementation: test all possible targets against each argument, only
-#   run the target if the arg matches the name. Basically, a Bash makefile...)
-# another possibility: actually make it a makefile!
+# return values:
+# 1 - not enough arguments
+# 2 - the action was passed incorrectly
 
 # get the location of the script from the call
 REPO_DIR=$(dirname $(readlink -f "$0"))
-ARGS="$@"
-HELP=1
+for (( i=2; i<=$#; i+=1 )); do
+  ARGS="$ARGS ${!i}"
+done
 
 # functions for logging warnings and errors
 info () { echo -e "\x1B[32m[INF] " $@ "\x1B[0m"; }
@@ -14,31 +14,87 @@ warn () { echo -e "\x1B[33m[WRN] " $@ "\x1B[0m"; }
 error () { echo -e "\x1B[31m[ERR] " $@ "\x1B[0m"; }
 
 # various useful commands
-symlink () { if [ -e $2 ]; then warn $2 "already exists, skipping"; else ln -vs $1 $2; fi }
+# we use hardlinks b/c they take up less space, are faster, and we don't need symlink benefits
+hardlink () { if [[ -e $2 ]]; then warn $2 "already exists, skipping"; else ln -v $1 $2; fi }
+hardlink_sudo () { if sudo bash -c "[[ -e $2 ]]"; then warn $2 "already exists, skipping"; else sudo ln -v $1 $2; fi }
+remove () { if [[ -e $1 ]]; then rm -r $1; else warn $1 "doesn't exist, skipping"; fi }
+remove_sudo () { if sudo bash -c "[[ -e $1 ]]"; then sudo rm -r $1; else warn $1 "doesn't exist, skipping"; fi }
 MKDIR_CMD="mkdir -vp"
 
-# set up vim
-if [[ $ARGS =~ vim ]] || [[ $ARGS =~ all ]]; then
-  info "Setting up Vim config..."
-  symlink $REPO_DIR/vim ~/.vim
-  symlink $REPO_DIR/vim/.vimrc ~/.vimrc
-  info "Don't forget to compile YCM!"
-  HELP=0
-fi
-
-# set up sway
-if [[ $ARGS =~ sway ]] || [[ $ARGS =~ all ]]; then
-  info "Setting up Sway config..."
-  $MKDIR_CMD ~/.config
-  symlink $REPO_DIR/sway ~/.config/sway
-  HELP=0
-fi
-
-# help text, if nothing above ran
-if [ $HELP == 1 ]; then
+# print help text
+helptext () {
   info "npiscitello's configuration installer"
-  info "args:"
-  info "\tall - install all configs (as if you passed every argument below)"
-  info "\tvim - installs the ~/.vim directory and the ~/.vimrc file"
-  info "\tswy - installs the Sway setup in the ~/.config directory"
+  info "usage: ./setup.sh [ action ] [ component ] [ component ] ..."
+  info ""
+  info "actions (exactly one must be present):"
+  info "\tinstall - hardlink relevant files to the filesystem"
+  info "\tremove  - delete configs of the specified components"
+  info "\t\tWARNING: this tool will not differentiate between configs installed"
+  info "\t\tby itself and custom user configs. Use with caution!"
+  info ""
+  info "components (at least one must be present):"
+  info "\tall  - all configs (as if you passed every argument below)"
+  info "\tvim  - the ~/.vim directory and the ~/.vimrc file"
+  info "\tsway - the Sway setup in the ~/.config directory"
+  info "\tudev - udev rules in /etc/udev/rules.d (calls sudo)"
+  exit $1
+}
+
+# check that enough arguments were passed
+if [[ $# -lt 2 ]]; then
+  helptext 1
+fi
+
+# parse out action
+if [[ $1 -eq "install" ]]; then
+  INSTALL=0
+else if [[ $1 -ne "remove" ]]; then
+  INSTALL=1
+else
+  helptext 2
+fi
+
+# Vim
+if [[ $ARGS =~ vim ]] || [[ $ARGS =~ all ]]; then
+  if $INSTALL; then
+    info "Installing Vim config..."
+    hardlink $REPO_DIR/vim ~/.vim
+    hardlink $REPO_DIR/vim/.vimrc ~/.vimrc
+    info "Don't forget to compile YCM!"
+  else
+    info "Removing Vim config..."
+    remove ~/.vim
+    remove ~/.vimrc
+  fi
+fi
+
+# Sway
+if [[ $ARGS =~ sway ]] || [[ $ARGS =~ all ]]; then
+  if $INSTALL; then
+    info "Installing Sway config..."
+    $MKDIR_CMD ~/.config
+    hardlink $REPO_DIR/sway ~/.config/sway
+  else
+    info "Removing Sway config..."
+    remove ~/.config/sway
+  fi
+fi
+
+# udev
+if [[ $ARGS =~ udev ]] || [[ $ARGS =~ all ]]; then
+  if $INSTALL; then
+    info "Installing udev rules (calls sudo)..."
+    cd udev
+    for rule in *; do
+      hardlink_sudo $rule /etc/udev/rules.d/$rule
+    done
+    cd ..
+  else
+    info "Removing udev rules (calls sudo)..."
+    cd udev
+    for rule in *; do
+      remove_sudo /etc/udev/rules.d/$rule
+    done
+    cd ..
+  fi
 fi
